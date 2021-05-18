f8e693a5f07b88d6a375455f6866e25edfc01643
akbaraadis <akbaradis99@gmail.com>
2021-05-18 19:46:41 +0700

    added font

--- app/src/main/java/com/project/sapaai/ui/HomeActivity.kt
+++ app/src/main/java/com/project/sapaai/ui/HomeActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.project.sapaai.R
 
 class HomeActivity : AppCompatActivity() {
 
--- app/src/main/java/com/project/sapaai/ui/home_form_profile/home/HomeFragment.kt
+++ app/src/main/java/com/project/sapaai/ui/home_form_profile/home/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.project.sapaai.ui.R
+import com.project.sapaai.R
 
 class HomeFragment : Fragment() {
 
