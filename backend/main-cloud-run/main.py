ef991fa1486c3241b742f384dac63625deb585f7
Hazunanafaru <hazunanafaru@gmail.com>
2021-06-09 13:16:47 +0700

    Fix call and change PUT into POST

--- backend/main-cloud-run/main.py
+++ backend/main-cloud-run/main.py
@@ -2,8 +2,7 @@
 # Import packages
 import os
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from google.auth.environment_vars import PROJECT
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -27,7 +26,7 @@ firebase_admin.initialize_app(cred, {
 
 # Set firebase RDb references
 forms = db.reference('forms/')
-panick = db.reference('panick/')
+call = db.reference('call/')
 process = db.reference('process/')
 users = db.reference('users/')
 
@@ -35,22 +34,22 @@ users = db.reference('users/')
 form_get_args = reqparse.RequestParser()
 form_get_args.add_argument("id_forms", type=str, help="Forms ID is required", required=True)
 
-form_put_args = reqparse.RequestParser()
-form_put_args.add_argument("id_users", type=str, help="Users ID is required", required=True)
-form_put_args.add_argument("nama_korban_forms", type=str, help="Victim name is required", required=True)
-form_put_args.add_argument("nama_tersangka_forms", type=str, help="Suspect name is required", required=True)
-form_put_args.add_argument("kronologi_forms", type=str, help="Description is required", required=True)
-form_put_args.add_argument("flag_layanan1", type=bool, help="Service 1")
-form_put_args.add_argument("flag_layanan2", type=bool, help="Service 2")
-form_put_args.add_argument("flag_layanan3", type=bool, help="Service 3")
-form_put_args.add_argument("flag_layanan4", type=bool, help="Service 4")
-form_put_args.add_argument("flag_layanan5", type=bool, help="Service 5")
-form_put_args.add_argument("flag_layanan6", type=bool, help="Service 6")
-form_put_args.add_argument("flag_layanan7", type=bool, help="Service 7")
-form_put_args.add_argument("flag_layanan8", type=bool, help="Service 8")
-form_put_args.add_argument("foto_forms", type=str, help="Photos")
-form_put_args.add_argument("lokasi_forms", type=str, help="Location is required", required=True)
-form_put_args.add_argument("upload_date_forms", type=str, help="Upload time is required", required=True)
+form_post_args = reqparse.RequestParser()
+form_post_args.add_argument("id_users", type=str, help="Users ID is required", required=True)
+form_post_args.add_argument("nama_korban_forms", type=str, help="Victim name is required", required=True)
+form_post_args.add_argument("nama_tersangka_forms", type=str, help="Suspect name is required", required=True)
+form_post_args.add_argument("kronologi_forms", type=str, help="Description is required", required=True)
+form_post_args.add_argument("flag_layanan1", type=bool, help="Service 1")
+form_post_args.add_argument("flag_layanan2", type=bool, help="Service 2")
+form_post_args.add_argument("flag_layanan3", type=bool, help="Service 3")
+form_post_args.add_argument("flag_layanan4", type=bool, help="Service 4")
+form_post_args.add_argument("flag_layanan5", type=bool, help="Service 5")
+form_post_args.add_argument("flag_layanan6", type=bool, help="Service 6")
+form_post_args.add_argument("flag_layanan7", type=bool, help="Service 7")
+form_post_args.add_argument("flag_layanan8", type=bool, help="Service 8")
+form_post_args.add_argument("foto_forms", type=str, help="Photos")
+form_post_args.add_argument("lokasi_forms", type=str, help="Location is required", required=True)
+form_post_args.add_argument("upload_date_forms", type=str, help="Upload time is required", required=True)
 
 # Form Resource Fields
 form_get_resource_fields = {
@@ -71,29 +70,34 @@ form_get_resource_fields = {
     'upload_date_forms': fields.String
     }
 
-forms_put_resource_fields = {
+forms_post_resource_fields = {
     'id_forms': fields.String,
     }
 
 # From Service
-class Form(Resource):
+class Forms_get(Resource):
     """
     Build Form API to
     1. GET report from database
-    2. PUT report from end users
-    3. DELETE report from database by end users
     """
 
-    # @marshal_with(form_get_resource_fields)
+    @marshal_with(form_get_resource_fields)
     def get(self):
         args = form_get_args.parse_args()
         down_forms_ref = db.reference('forms/' + str(args['id_forms']))
         down_forms = down_forms_ref.get()
         return down_forms, 200
 
-    @marshal_with(forms_put_resource_fields)
-    def put(self):
-        args = form_put_args.parse_args()
+# From Service
+class Forms_post(Resource):
+    """
+    Build Form API to
+    2. PUT report from end users
+    """
+
+    @marshal_with(forms_post_resource_fields)
+    def post(self):
+        args = form_post_args.parse_args()
         data = {"id_users":args['id_users'],
                 "nama_korban_forms":args['nama_korban_forms'],
                 "nama_tersangka_forms":args['nama_tersangka_forms'],
@@ -113,32 +117,39 @@ class Form(Resource):
         up_forms = forms.push(data)
         id_forms = {"id_forms": up_forms.key}
         return id_forms, 201
+
+# From Service
+# class Forms_delete(Resource):
+#     """
+#     Build Form API to
+#     3. DELETE report from database by end users
+#     """
     
-    # def delete(self, id_forms):
-    #     # Masih development
-    #     result = FormsModel.query.filter_by(id_forms=id_forms).first()
-    #     if not result:
-    #         abort(404, message=f"Could not find the report with ID: {id_forms}...")
-    #     forms = FormsModel.query.filter_by(id_forms=id_forms).delete()
-    #     db.session.add(forms)
-    #     db.session.commit()
-    #     return f"Form with ID: {id_forms} successfully deleted", 204
+#     def delete(self, id_forms):
+#         # Masih development
+#         result = FormsModel.query.filter_by(id_forms=id_forms).first()
+#         if not result:
+#             abort(404, message=f"Could not find the report with ID: {id_forms}...")
+#         forms = FormsModel.query.filter_by(id_forms=id_forms).delete()
+#         db.session.add(forms)
+#         db.session.commit()
+#         return f"Form with ID: {id_forms} successfully deleted", 204
 
-# Panick Arguments
-panick_get_args = reqparse.RequestParser()
-panick_get_args.add_argument("id_panick", type=str, help="Panick ID is required", required=True)
-panick_get_args.add_argument("id_process", type=str, help="Process ID is required", required=True)
+# call Arguments
+call_get_args = reqparse.RequestParser()
+call_get_args.add_argument("id_call", type=str, help="call ID is required", required=True)
+call_get_args.add_argument("id_process", type=str, help="Process ID is required", required=True)
 
-panick_put_args = reqparse.RequestParser()
-panick_put_args.add_argument("id_users", type=str, help="Users ID is required", required=True)
-panick_put_args.add_argument("record_panick", type=str, help="Record URI is required", required=True)
-panick_put_args.add_argument("upload_date_panick", type=str, help="Upload time is required", required=True)
+call_post_args = reqparse.RequestParser()
+call_post_args.add_argument("id_users", type=str, help="Users ID is required", required=True)
+call_post_args.add_argument("record_call", type=str, help="Record URI is required", required=True)
+call_post_args.add_argument("upload_date_call", type=str, help="Upload time is required", required=True)
 
-# Panick Resource Fields
-panick_resource_fields = {
-    'id_panick': fields.String,
+# call Resource Fields
+call_resource_fields = {
+    'id_call': fields.String,
     'id_process': fields.String,
-    'upload_date_panick': fields.String,
+    'upload_date_call': fields.String,
     'flag_layanan1': fields.Boolean,
     'flag_layanan2': fields.Boolean,
     'flag_layanan3': fields.Boolean,
@@ -149,32 +160,30 @@ panick_resource_fields = {
     'flag_layanan8': fields.Boolean
     }
 
-panick_put_resource_fields = {
-    'id_panick': fields.String,
+call_post_resource_fields = {
+    'id_call': fields.String,
     'id_process': fields.String
     }
 
 # From Service
-class Panick(Resource):
+class Call_get(Resource):
     """
-    Build Panick API to
+    Build call API to
     1. GET report from database
-    2. PUT report from end users
-    3. DELETE report from database by end users
     """
 
-    @marshal_with(panick_resource_fields)
+    @marshal_with(call_resource_fields)
     def get(self):
-        args = panick_get_args.parse_args()
-        down_panick_ref = db.reference('panick/' + str(args['id_panick']))
+        args = call_get_args.parse_args()
+        down_call_ref = db.reference('call/' + str(args['id_call']))
         down_process_ref = db.reference('process/' + str(args['id_process']))
-        down_panick = down_panick_ref.get()
+        down_call = down_call_ref.get()
         down_process = down_process_ref.get()
 
         # Query Return
-        send_data={"id_panick":args['id_panick'],
+        send_data={"id_call":args['id_call'],
                     "id_process":args['id_process'],
-                    "upload_date_panick":down_panick['upload_date_panick'],
+                    "upload_date_call":down_call['upload_date_call'],
                     "flag_layanan1":down_process['flag_layanan1'],
                     "flag_layanan2":down_process['flag_layanan2'],
                     "flag_layanan3":down_process['flag_layanan3'],
@@ -186,24 +195,31 @@ class Panick(Resource):
                     }
         return send_data, 200
 
-    @marshal_with(panick_put_resource_fields)
-    def put(self):
-        args_panick = panick_put_args.parse_args()
-        data = {"id_users":args_panick['id_users'],
-                "record_panick":args_panick['record_panick'],
-                "upload_date_panick":args_panick['upload_date_panick']
+# From Service
+class Call_post(Resource):
+    """
+    Build call API to
+    2. PUT report from end users
+    """
+
+    @marshal_with(call_post_resource_fields)
+    def post(self):
+        args_call = call_post_args.parse_args()
+        data = {"id_users":args_call['id_users'],
+                "record_call":args_call['record_call'],
+                "upload_date_call":args_call['upload_date_call']
         }
-        up_panick = panick.push(data)
-        id_panick = up_panick.key
+        up_call = call.push(data)
+        id_call = up_call.key
 
         # Start Processing Recorded Voices
-        text = transcribe_sound(args_panick['record_panick'])
-        instances=[text['transcript']]
+        text = transcribe_sound(args_call['record_call'])
+        instances=[text]
         version="v3"
         pred_test = predict_json(instances, version)
 
         result = {
-            "id_panick":id_panick,
+            "id_call":id_call,
             "result_process":instances[0],
             "flag_layanan1":pred_test[0][0],
             "flag_layanan2":pred_test[0][1],
@@ -219,32 +235,43 @@ class Panick(Resource):
         
         
         # Query Return
-        send_data={"id_panick":id_panick,
+        send_data={"id_call":id_call,
                     "id_process":id_process
                     }
 
         return send_data, 201
 
-    # def delete(self, id_panick):
-    #     # Masih development
-    #     result = FormsModel.query.filter_by(id_panick=id_panick).first()
-    #     if not result:
-    #         abort(404, message=f"Could not find the report with ID: {id_panick}...")
-    #     forms = FormsModel.query.filter_by(id_panick=id_panick).delete()
-    #     db.session.add(forms)
-    #     db.session.commit()
-    #     return f"Form with ID: {id_panick} successfully deleted", 204
+# # From Service
+# class Call_delete(Resource):
+#     """
+#     Build call API to
+#     3. DELETE report from database by end users
+#     """
+
+#     def delete(self, id_call):
+#         # Masih development
+#         result = FormsModel.query.filter_by(id_call=id_call).first()
+#         if not result:
+#             abort(404, message=f"Could not find the report with ID: {id_call}...")
+#         forms = FormsModel.query.filter_by(id_call=id_call).delete()
+#         db.session.add(forms)
+#         db.session.commit()
+#         return f"Form with ID: {id_call} successfully deleted", 204
+
 
 class Home(Resource):
     def get(self):
-        return "Welcome to Sapa.AI API!\nYou can use our Foms Service or Panick Service", 200
+        return "Welcome to Sapa.AI API! You can use our Foms Service or Call Service", 200
 
 api.add_resource(Home, '/')
-api.add_resource(Form, '/forms')
-api.add_resource(Panick, '/panick')
+api.add_resource(Forms_get, '/forms/get')
+api.add_resource(Forms_post, '/forms/post')
+api.add_resource(Call_get, '/call/get')
+api.add_resource(Call_post, '/call/post')
 
 # Test Main
 if __name__ == "__main__":
+    # app.run(debug=True)
     app.run()
 
 # # Deploy Main
